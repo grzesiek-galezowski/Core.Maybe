f6180334da833a904d9b745bfbfc05183c8ef0c4
William Casarin <jb55@jb55.com>
2018-12-01 12:49:46 -0800

    test: add enumerator tests
    These are pretty trivial, just for sanity checking
    
    Signed-off-by: William Casarin <jb55@jb55.com>

---
+++ Functional.Maybe.Tests/MaybeEnumeratorTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional.Maybe.Tests
+{
+	[TestClass]
+	public class MaybeEnumeratorTests
+	{
+		[TestMethod]
+		public void MaybeWithValueEnumerates()
+		{
+			var m = 1.ToMaybe();
+			int c = 0;
+			foreach (var val in m)
+				c++;
+			foreach (var val in m)
+				c++;
+			Assert.IsTrue(c == 2);
+		}
+
+		[TestMethod]
+		public void EmptyDoesntEnumerate()
+		{
+			bool gotHere = false;
+			foreach (var val in Maybe<bool>.Nothing)
+				gotHere = true;
+			Assert.IsFalse(gotHere);
+		}
+	}
+}
