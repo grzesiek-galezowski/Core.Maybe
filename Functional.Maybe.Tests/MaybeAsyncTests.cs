495e30807db6e6102f72fd46869d5052fd222b0f
Andrey Tsvetkov <ts.andrey@gmail.com>
2018-12-19 14:23:22 +0300

    Basic async support for Maybe<T>: SelectAsync

---
+++ Functional.Maybe.Tests/MaybeAsyncTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Functional.Maybe.Tests
+{
+	[TestClass]
+	public class MaybeAsyncTests
+	{
+		[TestMethod]
+		public async void SelectAsyncTest()
+		{
+			Task<int> two() => Task.FromResult(2);
+
+			var onePlusTwo = await 1.ToMaybe().SelectAsync(async one => one + (await two()));
+
+			Assert.AreEqual(3, onePlusTwo);
+		}
+	}
+}
---
+++ Functional.Maybe/MaybeAsync.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Functional.Maybe
+{
+	public static class MaybeAsync
+	{
+		/// <summary>
+		/// Flips Maybe and Task: instead of having Maybe<Task<T>>> (as in case of Select) we get Task<Maybe<T>> and have possibility to await on it.
+		/// </summary>
+		/// <typeparam name="T">source type</typeparam>
+		/// <typeparam name="TR">async result type</typeparam>
+		/// <param name="this">maybe to map</param>
+		/// <param name="res">async mapper</param>
+		/// <returns>Task of Maybe of TR</returns>
+		public static async Task<Maybe<TR>> SelectAsync<T, TR>(this Maybe<T> @this, Func<T, Task<TR>> res) => @this.HasValue
+			? (await res(@this.Value)).ToMaybe()
+			: (default);
+		public static async Task<T> OrElseAsync<T>(this Task<Maybe<T>> @this, Func<Task<T>> orElse)
+		{
+			var res = await @this;
+			return res.HasValue ? res.Value : await orElse();
+		}
+		public static async Task<T> OrElse<T>(this Task<Maybe<T>> @this, T orElse)
+		{
+			var res = await @this;
+			return res.HasValue ? res.Value : orElse;
+		}
+		public static async Task<T> OrElse<T>(this Task<Maybe<T>> @this, Func<T> orElse)
+		{
+			var res = await @this;
+			return res.HasValue ? res.Value : orElse();
+		}
+	}
+}
