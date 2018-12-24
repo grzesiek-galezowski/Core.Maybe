97ac5cebe2eb58251a4fc7b6f9e6667749d640f4
Andrey Tsvetkov <ts.andrey@gmail.com>
2018-12-24 12:51:53 +0300

    added test for the ToNullable bug

---
+++ Functional.Maybe.Tests/MaybeNullableTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Functional.Maybe.Tests
+{
+	[TestClass]
+	public class MaybeNullableTests
+	{
+		[TestMethod]
+		public void ToNullableTest()
+		{
+			var nothing = Maybe<Guid>.Nothing;
+			Assert.AreEqual(null, nothing.ToNullable());
+		}
+	}
+}
