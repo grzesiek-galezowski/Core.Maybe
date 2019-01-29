df4aac0cf3ba1f0b5d9f6bc3b310543c70c818e8
tima <n-tima@ya.ru>
2019-01-30 02:34:36 +0300

    Initial-add-either-commit

---
+++ Functional.Maybe.Tests/EitherTests.cs
@@ -0,0 +1,191 @@
+﻿using Functional.Maybe.Either;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Functional.Maybe.Tests
+{
+    [TestClass]
+    public class EitherTests
+    {
+        private readonly Either<int, string> _eitherLeft;
+        private readonly Either<int, string> _eitherRight;
+        private const int _eitherLeftValue = 5;
+        private const string _eitherRightValue = "Five";
+
+        public EitherTests()
+        {
+            _eitherLeft = new Either<int, string>(_eitherLeftValue);
+            _eitherRight = new Either<int, string>(_eitherRightValue);
+        }
+
+        [TestMethod]
+        public void NullCheckingTests()
+        {
+            Action<int> nullActionInt = null;
+            Action<int> mockActionInt = x => { var y = 5; };
+
+            Action<string> nullActionString = null;
+            Action<string> mockActionString = x => { var y = 5; };
+
+            Action nullAction = null;
+            Action mockAction = () => { var a = 1; };
+
+            AssertExtension.Throws<ArgumentNullException>(() => _eitherLeft.Match(nullAction, mockAction));
+            AssertExtension.Throws<ArgumentNullException>(() => _eitherLeft.Match(mockAction, nullAction));
+            _eitherLeft.Match(mockAction, mockAction);
+
+            AssertExtension.Throws<ArgumentNullException>(() => _eitherRight.Match(nullAction, mockAction));
+            AssertExtension.Throws<ArgumentNullException>(() => _eitherRight.Match(mockAction, nullAction));
+            _eitherRight.Match(mockAction, mockAction);
+
+            AssertExtension.Throws<ArgumentNullException>(() => _eitherRight.Match(nullActionInt, mockActionString));
+            AssertExtension.Throws<ArgumentNullException>(() => _eitherRight.Match(mockActionInt, nullActionString));
+            _eitherLeft.Match(mockActionInt, mockActionString);
+        }
+
+        [TestMethod]
+        public void MatchActionTests()
+        {
+            var bool1 = false;
+            var bool2 = false;
+            var testInt = 0;
+            var testString = null as string;
+
+            Action resetTestValues = () =>
+            {
+                bool1 = false;
+                bool2 = false;
+                testInt = 0;
+                testString = null;
+            };
+
+            Action setBool1Action = () => bool1 = true;
+            Action setBool2Action = () => bool2 = true;
+
+            Action<int> setTestInt = value => testInt = value;
+            Action<string> setTestString = value => testString = value;
+
+            _eitherLeft.Match(setBool1Action, setBool2Action);
+            Assert.IsTrue(bool1);
+            Assert.IsFalse(bool2);
+
+            resetTestValues();
+            _eitherRight.Match(setBool1Action, setBool2Action);
+            Assert.IsFalse(bool1);
+            Assert.IsTrue(bool2);
+
+            resetTestValues();
+            _eitherLeft.Match(setTestInt, setTestString);
+            Assert.AreEqual(_eitherLeftValue, testInt);
+            Assert.AreEqual(null, testString);
+
+            resetTestValues();
+            _eitherRight.Match(setTestInt, setTestString);
+            Assert.AreEqual(0, testInt);
+            Assert.AreEqual(_eitherRightValue, testString);
+
+            resetTestValues();
+        }
+
+        [TestMethod]
+        public void MatchFunctionTests()
+        {
+            var testInt = 0;
+            var testString = null as string;
+
+            Action resetTestValues = () =>
+            {
+                testInt = 0;
+                testString = null;
+            };
+
+            Func<int, bool> funcTLT = x =>
+            {
+                testInt = x;
+                return true;
+            };
+
+            Func<string, bool> funcTRT = x =>
+            {
+                testString = x;
+                return false;
+            };
+
+            Assert.IsTrue(_eitherLeft.Match(funcTLT, funcTRT));
+            Assert.AreEqual(_eitherLeftValue, testInt);
+            Assert.AreEqual(null, testString);
+
+            resetTestValues();
+            Assert.IsFalse(_eitherRight.Match(funcTLT, funcTRT));
+            Assert.AreEqual(_eitherRightValue, testString);
+            Assert.AreEqual(0, testInt);
+
+            resetTestValues();
+            Assert.IsTrue(_eitherLeft.Match(() => true, () => false));
+            Assert.IsFalse(_eitherRight.Match(() => true, () => false));
+        }
+
+        [TestMethod]
+        public void LeftOrDefaultAndRightOrDefaultTests()
+        {
+            Assert.AreEqual(_eitherLeftValue, _eitherLeft.LeftOrDefault());
+            Assert.AreEqual(_eitherRightValue, _eitherRight.RightOrDefault());
+
+            Assert.AreEqual(default, _eitherRight.LeftOrDefault());
+            Assert.AreEqual(default, _eitherLeft.RightOrDefault());
+
+            Assert.AreEqual(29, _eitherRight.LeftOrDefault(29));
+            Assert.AreEqual("Twenty nine", _eitherLeft.RightOrDefault("Twenty nine"));
+        }
+
+        [TestMethod]
+        public void SameTLTRTests()
+        {
+            var eitherLeft = Either<string, string>.CreateLeft("Left defined");
+            var eitherRight = Either<string, string>.CreateRight("Right defined");
+
+            Assert.AreEqual("Left defined", eitherLeft.LeftOrDefault());
+            Assert.AreEqual("Right defined", eitherRight.RightOrDefault());
+
+            Assert.AreEqual(null, eitherRight.LeftOrDefault());
+            Assert.AreEqual(null, eitherLeft.RightOrDefault());
+        }
+
+        [TestMethod]
+        public void ExtensionMethodTests()
+        {
+            var eitherLeft = 29.ToEitherLeft<int, string>();
+            var eitherRight = "Twenty nine".ToEitherRight<int, string>();
+
+            Assert.AreEqual(29, eitherLeft.LeftOrDefault());
+            Assert.AreEqual(null, eitherLeft.RightOrDefault());
+
+            Assert.AreEqual("Twenty nine", eitherRight.RightOrDefault());
+            Assert.AreEqual(0, eitherRight.LeftOrDefault());
+        }
+    }
+
+
+    class AssertExtension
+    {
+        public static void Throws<T>(Action action) where T : Exception
+        {
+            var exceptionThrown = false;
+            try
+            {
+                action.Invoke();
+            }
+            catch (T)
+            {
+                exceptionThrown = true;
+            }
+
+            if (!exceptionThrown)
+            {
+                throw new AssertFailedException(
+                    String.Format("An exception of type {0} was expected, but not thrown", typeof(T))
+                    );
+            }
+        }
+    }
+}
---
+++ Functional.Maybe/Either/Either.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace Functional.Maybe.Either
+{
+    /// <summary>
+    /// A functional monadic concept Either to make validation code more expressive and easier to maintain.
+    /// <typeparam name="TL">Type of "Left" item</typeparam>
+    /// <typeparam name="TR">Type of "Right" item</typeparam>
+    /// </summary>
+    public class Either<TL, TR>
+    {
+        private readonly TL _left;
+        private readonly TR _right;
+        private readonly bool _isLeft;
+
+        /// <summary>
+        /// Constructs new <see cref="Either{TL, TR}"/> with left part defined
+        /// </summary>
+        public Either(TL left)
+        {
+            _left = left;
+            _isLeft = true;
+        }
+
+        /// <summary>
+        /// Constructs new <see cref="Either{TL, TR}"/> with right part defined
+        /// </summary>
+        /// <param name="right"></param>
+        public Either(TR right)
+        {
+            _right = right;
+            _isLeft = false;
+        }
+
+        private Either(TL left, TR right, bool isLeft)
+        {
+            _isLeft = isLeft;
+
+            if (isLeft)
+            {
+                _left = left;
+            }
+            else
+            {
+                _right = right;
+            }
+        }
+
+        /// <summary>
+        ///  Constructs new <see cref="Either{TL, TR}"/> with left part defined.
+        ///  Suitable for those Either instances, where TL and TR are same.
+        /// </summary>
+        public static Either<TL, TR> CreateLeft(TL left)
+        {
+            return new Either<TL, TR>(left, default, true);
+        }
+
+        /// <summary>
+        ///  Constructs new <see cref="Either{TL, TR}"/> with right part defined.
+        ///  Suitable for those Either instances, where TL and TR are same.
+        /// </summary>
+        public static Either<TL, TR> CreateRight(TR right)
+        {
+            return new Either<TL, TR>(default, right, false);
+        }
+
+        /// <summary>
+        /// Executes left or right function depending on the Either state.
+        /// </summary>
+        public T Match<T>(Func<TL, T> leftFunc, Func<TR, T> rightFunc)
+        {
+            if (leftFunc == null)
+            {
+                throw new ArgumentNullException(nameof(leftFunc));
+            }
+
+            if (rightFunc == null)
+            {
+                throw new ArgumentNullException(nameof(rightFunc));
+            }
+
+            return _isLeft ? leftFunc(_left) : rightFunc(_right);
+        }
+
+        /// <summary>
+        /// Executes left or right function depending on the Either state.
+        /// </summary>
+        public T Match<T>(Func<T> leftFunc, Func<T> rightFunc)
+        {
+            if (leftFunc == null)
+            {
+                throw new ArgumentNullException(nameof(leftFunc));
+            }
+
+            if (rightFunc == null)
+            {
+                throw new ArgumentNullException(nameof(rightFunc));
+            }
+
+            return _isLeft ? leftFunc() : rightFunc();
+        }
+
+        /// <summary>
+        /// Executes left or right action depending on the Either state.
+        /// </summary>
+        public void Match(Action<TL> leftAction, Action<TR> rightAction)
+        {
+            if (leftAction == null)
+            {
+                throw new ArgumentNullException(nameof(leftAction));
+            }
+
+            if (rightAction == null)
+            {
+                throw new ArgumentNullException(nameof(rightAction));
+            }
+
+            if (_isLeft)
+            {
+                leftAction(_left);
+            }
+            else
+            {
+                rightAction(_right);
+            }
+        }
+
+        /// <summary>
+        /// Executes left or right action depending on the Either state.
+        /// </summary>
+        public void Match(Action leftAction, Action rightAction)
+        {
+            if (leftAction == null)
+            {
+                throw new ArgumentNullException(nameof(leftAction));
+            }
+
+            if (rightAction == null)
+            {
+                throw new ArgumentNullException(nameof(rightAction));
+            }
+
+            if (_isLeft)
+            {
+                leftAction();
+            }
+            else
+            {
+                rightAction();
+            }
+        }
+
+        public TL LeftOrDefault() => Match(l => l, r => default);
+        public TR RightOrDefault() => Match(l => default, r => r);
+        public TL LeftOrDefault(TL value) => Match(l => l, r => value);
+        public TR RightOrDefault(TR value) => Match(l => value, r => r);
+
+        public static implicit operator Either<TL, TR>(TL left) => new Either<TL, TR>(left);
+        public static implicit operator Either<TL, TR>(TR right) => new Either<TL, TR>(right);
+    }
+
+    public static class EitherExtensions
+    {
+        public static Either<TL, TR> ToEitherLeft<TL, TR>(this TL left) => new Either<TL, TR>(left);
+        public static Either<TL, TR> ToEitherRight<TL, TR>(this TR right) => new Either<TL, TR>(right);
+    }
+}
